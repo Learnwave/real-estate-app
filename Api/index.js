24d6e4ef84db34a39ae644855dfd5e7d02de88eb
Learnwave <146640293+Learnwave@users.noreply.github.com>
2024-11-25 14:20:13 +0530

    connect to database

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect("mongodb://localhost:27017/real-estate-app").then(()=>{console.log("DB Connected Successfully")}).catch((err)=>{
+    console.log();
+    
+})
 
 const app = express();
 
